045e04b52f406f046b8d1b10511aa51c36da9008
Md Sulaiman <51925710+sulaiman-coder@users.noreply.github.com>
2023-09-06 19:51:26 +0600

    Update shell.go

--- shell.go
+++ shell.go
@@ -8,8 +8,8 @@ import (
 	"strings"
 	"sync"
 
-	"github.com/gorillalabs/go-powershell/backend"
-	"github.com/gorillalabs/go-powershell/utils"
+	"github.com/khulnasoft-lab/go-powershell/backend"
+	"github.com/khulnasoft-lab/go-powershell/utils"
 	"github.com/juju/errors"
 )
 
